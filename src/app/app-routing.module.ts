114555b232982c8a97114e78123cd1e1e792d64a
Tuan Pham <luke.ken1993@gmail.com>
2021-12-13 00:43:43 +0100

    Updated style css, new libraries and components added in the app component along with package.json and other files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {PostsFeedComponent} from './posts-feed/posts-feed.component';
+import {PostsFeedComponent} from './components/posts-feed/posts-feed.component';
+import {PostEditComponent} from './components/post-edit/post-edit.component';
+import {PostDetailComponent} from './components/post-detail/post-detail.component';
+import {LoginComponent} from './components/login/login.component';
+import {AuthenticationGuard} from './guards/authentication.guard';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/posts-feed', pathMatch: 'full' },
-  { path: 'posts-feed', component: PostsFeedComponent },
+  { path: 'posts-feed', component: PostsFeedComponent, canActivate: [AuthenticationGuard] },
+  { path: 'post/:id', component: PostDetailComponent, canActivate: [AuthenticationGuard] },
+  { path: 'editor', component: PostEditComponent, canActivate: [AuthenticationGuard] },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HeaderComponent } from './header/header.component';
-import { PostsFeedComponent } from './posts-feed/posts-feed.component';
-import { PostComponent } from './posts-feed/post/post.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { MenubarModule } from 'primeng/menubar';
-import { InputTextModule } from 'primeng/inputtext';
-import {AvatarModule} from 'primeng/avatar';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostsFeedComponent } from './components/posts-feed/posts-feed.component';
+import { PostHeadlineComponent } from './components/posts-feed/post-headline/post-headline.component';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { LoginComponent } from './components/login/login.component';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import {EditorModule} from 'primeng/editor';
+import {InputTextModule} from 'primeng/inputtext';
+import {KeyFilterModule} from 'primeng/keyfilter';
+
+import {AuthenticationInterceptor} from './interceptors/authentication.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -20,19 +28,26 @@ import {AvatarModule} from 'primeng/avatar';
     SidebarComponent,
     HeaderComponent,
     PostsFeedComponent,
-    PostComponent
+    PostHeadlineComponent,
+    PostEditComponent,
+    PostDetailComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     ButtonModule,
-    CardModule,
-    MenubarModule,
+    EditorModule,
     InputTextModule,
-    AvatarModule
+    KeyFilterModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
