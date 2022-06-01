35228e2883e68be994a035422b045faa170df22d
tuanpham <tuan.pham.se@gmail.com>
2022-06-01 20:46:00 +0200

    Changed main view based on login status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import {Component, ViewEncapsulation} from '@angular/core';
 })
 export class AppComponent {
   title = 'learning-track-frontend-angular';
+
+  isLoginUrl: boolean;
+
+  constructor(private route: ActivatedRoute, private router: Router, private location: Location) {
+
+    location.onUrlChange((url) => {
+      this.isLoginUrl = (url === '/login');
+    });
+  }
+
 }
