26118f3af31f44e01125686391d9a2aeb9871179
tuanpham <tuan.pham.se@gmail.com>
2022-06-01 20:41:31 +0200

    Changed header view based on login status

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../../services/authentication.service';
+import {Router} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isLoginUrl: boolean;
+
+  constructor(private authService: AuthenticationService, private router: Router, private location: Location) {
+    location.onUrlChange((url) => {
+      this.isLoginUrl = (url === '/login');
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onLogOut(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
